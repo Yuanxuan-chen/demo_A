31887eab9491eeb2d68e69c6922989aed40191d6
Yuanxuan-chen <1045010589@qq.com>
2021-02-19 15:15:08 +0800

    docs: 修改注释，页面排版

--- test_case/run_start.py
+++ test_case/run_start.py
@@ -11,7 +11,7 @@ import shutil
 if __name__ == '__main__':
     #文件保存路径
     raw = '../report/raw_file'
-    report = '../report/report_file/' # + config.now_time # 需要保存的时候就去掉注释
+    report = '../report/report_file/' # + config.now_time # 需要保存的时候就去掉注释，不然下次运行会覆盖旧数据
 
     # 生成测试报告
     pytest.main(["test_login_page.py::TestLoginPage",
@@ -20,7 +20,9 @@ if __name__ == '__main__':
     "--alluredir={}".format(raw), "--clean-alluredir" ])
     # 将运行环境拷贝到测试报告中
     shutil.copy('../config/environment.properties', '{}/environment.properties'.format(raw))
+
     # 启动测试报告
     # os.system("allure serve {}".format(raw))
+
     # 测试报告转html，并保存
     os.system("allure generate {} -o {} --clean".format(raw, report))
